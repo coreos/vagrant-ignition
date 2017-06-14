2f47e371aaf98697f9212496cebb5dee49c188ae
Alexander Pavel <alex.pavel@coreos.com>
2017-06-14 09:41:05 -0700

    Remove unused config option

--- lib/vagrant-ignition/config.rb
+++ lib/vagrant-ignition/config.rb
@@ -3,7 +3,6 @@ module VagrantPlugins
     class Config < Vagrant.plugin("2", :config)
       attr_accessor :enabled
       attr_accessor :path
-      attr_accessor :ssh_part_location
       attr_accessor :config_obj
       attr_accessor :config_img
       attr_accessor :config_vmdk
@@ -13,7 +12,6 @@ module VagrantPlugins
       def initialize
         @enabled     = UNSET_VALUE
         @path        = UNSET_VALUE
-        @ssh_path    = UNSET_VALUE
         @config_obj  = UNSET_VALUE
         @config_img  = UNSET_VALUE
         @config_vmdk = UNSET_VALUE
@@ -27,7 +25,6 @@ module VagrantPlugins
         @config_img  = "config.img"  if @config_img  == UNSET_VALUE
         @config_vmdk = "config.vmdk" if @config_vmdk == UNSET_VALUE
         @hostname    = nil           if @hostname    == UNSET_VALUE
-        @ssh_path    = nil           if @ssh_path    == UNSET_VALUE
         @ip          = nil           if @ip          == UNSET_VALUE
       end
     end
