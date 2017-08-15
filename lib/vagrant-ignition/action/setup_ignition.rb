37dc98d36bfc88598daeac3ef00124bde3e8132b
Alexander Pavel <alex.pavel@coreos.com>
2017-08-15 13:44:04 -0700

    lib: add vmware support
    This adds a provider tag that can be set to vmware or virtualbox to
    correctly configure a vm. It defaults to "virtualbox".

--- lib/vagrant-ignition/action/setup_ignition.rb
+++ lib/vagrant-ignition/action/setup_ignition.rb
@@ -1,4 +1,5 @@
 require_relative 'vmdk_gen'
+require 'base64'
 
 module VagrantPlugins
   module Ignition
@@ -16,17 +17,34 @@ module VagrantPlugins
             return
           end
 
-          config_path = env[:machine].config.ignition.path
+          ignition_path = env[:machine].config.ignition.path
           drive_name = env[:machine].config.ignition.drive_name
           drive_root = env[:machine].config.ignition.drive_root
 
           hostname = env[:machine].config.ignition.hostname
           ip = env[:machine].config.ignition.ip
+          provider = env[:machine].config.ignition.provider
 
-          vmdk_gen(config_path, drive_name, drive_root, hostname, ip, env)
+          merge_ignition(ignition_path, hostname, ip, env, provider)
+          if provider == "virtualbox"
+            vmdk_gen(ignition_path, drive_name, drive_root, hostname, ip, env)
 
-          env[:machine].ui.info "Configuring Ignition Config Drive"
-          env[:machine].provider.driver.execute("storageattach", "#{env[:machine].id}", "--storagectl", "IDE Controller", "--device", "0", "--port", "1", "--type", "hdd", "--medium", "#{File.join(drive_root, (drive_name + ".vmdk"))}")
+            env[:machine].ui.info "Configuring Ignition Config Drive"
+            env[:machine].provider.driver.execute("storageattach", "#{env[:machine].id}", "--storagectl", "IDE Controller", "--device", "0", "--port", "1", "--type", "hdd", "--medium", "#{File.join(drive_root, (drive_name + ".vmdk"))}")
+          elsif provider == "vmware"
+            data = ""
+            if !ignition_path.nil?
+              data = File.read(ignition_path + ".merged")
+            else
+              data = File.read("config.ign.merged")
+            end
+            env[:machine].ui.info "Setting Ignition GuestInfo"
+
+            File.open(env[:machine].provider.driver.vmx_path.to_s, 'ab') do |file|
+              file.puts "guestinfo.coreos.config.data" + " = " + Base64.strict_encode64(data)
+              file.puts "guestinfo.coreos.config.data.encoding = base64"
+            end
+          end
 
           # Continue through the middleware chain
           @app.call(env)
--- lib/vagrant-ignition/plugin.rb
+++ lib/vagrant-ignition/plugin.rb
@@ -14,6 +14,12 @@ module VagrantPlugins
       action_hook(:setup_ignition, :machine_action_up) do |hook|
         require_relative "action/setup_ignition"
         hook.after VagrantPlugins::ProviderVirtualBox::Action::Import, Action::SetupIgnition
+        if defined?(HashiCorp::VagrantVMwareworkstation)
+          hook.after HashiCorp::VagrantVMwareworkstation::Action::Import, Action::SetupIgnition
+        end
+        if defined?(HashiCorp::VagrantVMwarefusion)
+          hook.after HashiCorp::VagrantVMwarefusion::Action::Import, Action::SetupIgnition
+        end
       end
     end
   end
